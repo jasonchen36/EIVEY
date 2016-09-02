e37fb17c805369d01764c63b509a2c16c04ac0bb
mstumpf818 <mstumpf@ellefsontech.com>
2016-09-02 09:22:24 +0100

    sharetribe - header - search bar base styling in place; logic stubbed

--- frontend/js/sharetribe/sharetribe.header.js
+++ frontend/js/sharetribe/sharetribe.header.js
@@ -2,10 +2,15 @@
 
     var $ = jQuery,
         userMenuToggle = $('#header-user-toggle-menu'),
-        userMenuAnchor = $('#header-user-desktop-anchor');
+        userMenuAnchor = $('#header-user-desktop-anchor'),
+        searchButtonSubmit = $('#search-button');
 
     this.init = function(){
         userMenuToggle.width(userMenuAnchor.outerWidth());
+
+        searchButtonSubmit.on('click touchend', function(){
+            console.log('search me');
+        });
     };
 
 }).apply(app.header);
