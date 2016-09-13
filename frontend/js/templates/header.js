b864e1b434f66dd764feb9ea9a9cf0101d698185
mstumpf818 <mstumpf@ellefsontech.com>
2016-09-13 21:04:46 +0100

    sharetribe - header - bug fix for search bar

--- frontend/js/templates/header.js
+++ frontend/js/templates/header.js
@@ -11,12 +11,9 @@
     function getSharetribeBaseUrl(){
         var baseUrl = [location.protocol, '//', location.host].join(''),
             sharetribeDirectory = '/shop';
-        if (baseUrl.indexOf('.com') !== -1) {
+        if (baseUrl.indexOf('.com') !== -1 || baseUrl.indexOf(':3000') !== -1) {
             //for local environment
             sharetribeDirectory = '';
-            if (baseUrl.indexOf(':3000') === -1){
-                sharetribeDirectory = ':3000';
-            }
         }
         if (baseUrl.charAt(baseUrl.length - 1) === '/') {
             baseUrl = baseUrl.substring(0,baseUrl.length-1);
@@ -25,33 +22,14 @@
     }
 
     function searchSubmit(activeSubmitButton){
-        var searchQueryKey = 'q',
-            searchValue = activeSubmitButton.prev().val(),
-            urlParameters = helpers.getUrlParameters();
-        if (searchValue.length > 0){
-            if (urlParameters.hasOwnProperty(searchQueryKey)){
-                urlParameters[searchQueryKey] = searchValue;
-                var urlParametersArray = [];
-                for (var key in urlParameters) {
-                    if (urlParameters.hasOwnProperty(key)) {
-                        urlParametersArray.push(key+'='+urlParameters[key]);
-                    }
-                }
-                window.location.href = getSharetribeBaseUrl()+'?'+urlParametersArray.join('&');
-            } else {
-                var delimiter = '?';
-                if (window.location.href.indexOf('?') !== -1){
-                    delimiter = '&';
-                }
-                window.location.href = getSharetribeBaseUrl()+delimiter+searchQueryKey+'='+searchValue;
-            }
-        }
+        var searchValue = activeSubmitButton.prev().val();
+        window.location.href = getSharetribeBaseUrl()+'?q='+searchValue;
     }
-    
+
     function openMenu(){
         animations.fadeIn(mobileMenu);
     }
-    
+
     function closeMenu(){
         animations.fadeOut(mobileMenu);
     }
